8b373274aeda62cfa7e1237f468405520d36513a
Marlon2132 <troffstas752@gmail.com>
2025-07-02 11:41:18 +0300

    Test commit for .gitignore build

--- biorythmes/build/Desktop_Qt_6_8_3_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_biorythms_take5_FILE D:/RK-PPMOP/OldSchizo/HHCrewgroup/biorythmes/build/Desktop_Qt_6_8_3_llvm_mingw_64_bit-Debug/biorythms_take5.exe)
-set(__QT_DEPLOY_TARGET_biorythms_take5_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_biorythms_take5_RUNTIME_DLLS D:/Cutie/6.8.3/llvm-mingw_64/bin/Qt6Widgets.dll;D:/Cutie/6.8.3/llvm-mingw_64/bin/Qt6Gui.dll;D:/Cutie/6.8.3/llvm-mingw_64/bin/Qt6Core.dll)
