27816446233ca1ac280b8c388a87d60dbf453c87
Andrei Batrameyeu <batrameev2@gmail.com>
2025-07-06 18:44:19 +0300

    fixed server not compiling in new directory

--- Garson/ServerGarson/build/Desktop_Qt_6_8_3_llvm_mingw_64_bit-Debug/release/moc_mainwindow.cpp
+++ Garson/ServerGarson/build/Desktop_Qt_6_8_3_llvm_mingw_64_bit-Debug/release/moc_mainwindow.cpp
@@ -38,11 +38,7 @@ struct qt_meta_tag_ZN10MainWindowE_t {};
 
 #ifdef QT_MOC_HAS_STRINGDATA
 static constexpr auto qt_meta_stringdata_ZN10MainWindowE = QtMocHelpers::stringData(
-    "MainWindow",
-    "startServer",
-    "",
-    "newConnection",
-    "readClientData"
+    "MainWindow"
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -54,22 +50,12 @@ Q_CONSTINIT static const uint qt_meta_data_ZN10MainWindowE[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       0,    0, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
-
- // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   32,    2, 0x08,    1 /* Private */,
-       3,    0,   33,    2, 0x08,    2 /* Private */,
-       4,    0,   34,    2, 0x08,    3 /* Private */,
-
- // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
 
        0        // eod
 };
@@ -82,13 +68,7 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_tag_ZN10MainWindowE_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
-        // method 'startServer'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'newConnection'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'readClientData'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
     >,
     nullptr
 } };
@@ -96,14 +76,9 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     auto *_t = static_cast<MainWindow *>(_o);
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        switch (_id) {
-        case 0: _t->startServer(); break;
-        case 1: _t->newConnection(); break;
-        case 2: _t->readClientData(); break;
-        default: ;
-        }
-    }
+    (void)_t;
+    (void)_c;
+    (void)_id;
     (void)_a;
 }
 
@@ -123,18 +98,6 @@ void *MainWindow::qt_metacast(const char *_clname)
 int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
-    if (_id < 0)
-        return _id;
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
-            qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
-    }
-    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
-            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 3;
-    }
     return _id;
 }
 QT_WARNING_POP
